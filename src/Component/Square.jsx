34ba0e18e833549067bc02d8e1ba4d23865b5b2d
Ibrahim Safiyan <ibrahimmuazzarm@gmail.com>
2023-06-25 11:50:39 +0100

    tic-tac-toe second commit

--- src/Component/Square.jsx
+++ src/Component/Square.jsx
@@ -1,5 +1,15 @@
-function Square({ value }) {
-  return <button className="value">{value}</button>;
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button onClick={handleClick} className="value">
+      {value}
+    </button>
+  );
 }
 
 export default Square;
